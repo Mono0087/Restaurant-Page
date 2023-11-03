fe8d22089362d30270c7d581ea0df30773fed843
MonoTwo <125167202+Mono0087@users.noreply.github.com>
2023-11-04 04:11:08 +0500

    Add tab-switching logic

--- src/index.js
+++ src/index.js
@@ -8,5 +8,28 @@ import "./SCSS/style.scss"
 
 loadHeader()
 const main = createElement('main')
+const headerBtns = document.querySelector('.buttons-container')
 document.querySelector('#content').append(main)
-loadContactModule()
+loadHomeModule()
+
+headerBtns.addEventListener('click', btnsHandler)
+function btnsHandler(e) {
+    if (e.target.nodeName === 'BUTTON') {
+        let btnId = e.target.id
+        switch (btnId) {
+            case 'info-btn':
+                main.innerHTML = ''
+                loadContactModule()
+                break;
+            case 'menu-btn':
+                main.innerHTML = ''
+                loadMenuModule()
+                break;
+            case 'home-btn':
+                main.innerHTML = ''
+                loadHomeModule()
+            default:
+                break;
+        }
+    }
+}
